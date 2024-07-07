f981d312e7cb7fe23c39412ec8bfbd0c2f843254
Xiao Starlight <153096641+xiaoStarlight@users.noreply.github.com>
2024-07-07 19:24:46 +0800

    Update __init__.py
    测试

--- src/coolang/__init__.py
+++ src/coolang/__init__.py
@@ -1,4 +1,50 @@
-
 def load_cool_program(file: str):
-    print(file)
-    ...
+    variables={}
+    lines=file.strip().split('\n')
+    for line in lines:
+        if line.startswith('(return)'):
+            variable_name=line.strip('(return)').strip()
+            if variable_name in variables:
+                print(variables[variable_name])
+            else:
+                print("Undefined variable")
+        else:
+            eval_line(line,variables)
+
+
+def eval_line(line,variables):
+    if '+=' in line:
+        line="".join(line.split())
+        line = line.split('+=')
+        if line[1].isdigit():
+            variables[line[0]]+=int(line[1])
+        else:
+            variables[line[0]]+=variables(line[1])
+    elif '-=' in line:
+        line="".join(line.split())
+        line = line.split('-=')
+        if line[1].isdigit():
+            variables[line[0]]-=int(line[1])
+        else:
+            variables[line[0]]-=variables(line[1])
+    elif '*=' in line:
+        line="".join(line.split())
+        line = line.split('*=')
+        if line[1].isdigit():
+            variables[line[0]]*=int(line[1])
+        else:
+            variables[line[0]]*=variables(line[1])
+    elif '/=' in line:
+        line="".join(line.split())
+        line = line.split('/=')
+        if line[1].isdigit():
+            variables[line[0]]//=int(line[1])
+        else:
+            variables[line[0]]//=variables(line[1])
+    elif '=' in line:
+        line = "".join(line.split())
+        line=line.split('=')
+        if line[1].isdigit():
+            variables[line[0]]=int(line[1])
+        else:
+            variables[line[0]]=variables[line[1]]
